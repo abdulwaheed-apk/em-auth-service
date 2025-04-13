fc95f65ffe34542bb8ed177a799a2115e0d721c9
Abdul Waheed <ab.waheed1722@gmail.com>
2025-04-14 01:11:00 +0300

    signup complete and signin initial updates

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { SigninDto, SignupDto } from './dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from 'generated/prisma/runtime/library';
 
 @Injectable()
 export class AuthService {
-  signup() {
-    return 'sign up';
+  constructor(private prisma: PrismaService) {}
+
+  async signup(dto: SignupDto) {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+          firstName: dto.firstName,
+          lastName: dto.lastName,
+        },
+      });
+
+      const { password, ...userWithoutPassword } = user;
+      return userWithoutPassword;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  signin() {
-    return 'sign in';
+  async signin(dto: SigninDto) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+      if (!user) {
+        throw new NotFoundException('Account with this email not found');
+      }
+
+      const passMatched = await argon.verify(dto.password, user.password);
+      if (!passMatched) {
+        throw new ForbiddenException('Invalid Credentials');
+      }
+
+      const { password, ...userWithoutPassword } = user;
+      // generate token
+      return userWithoutPassword;
+    } catch (error) {
+      throw error;
+    }
   }
 }
